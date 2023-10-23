5abdaf873725156a6867d8c7318ffa42d59f4233
MaxiPrantera <maximiliano.prantera@gmail.com>
2023-10-23 20:23:56 -0300

    post actualizacion2

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -4,6 +4,9 @@ import alfalogo from "@/app/assets/alfalogo.png"
 import { redirect } from "next/navigation";
 import ShoppingCartButton from "./ShoppingCartButton";
 import { getCart } from "@/lib/db/cart";
+import  MenuButton  from "./MenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 async function searchProducts(formData: FormData){
     "use server";
@@ -17,6 +20,7 @@ async function searchProducts(formData: FormData){
 
 export default async function Navbar(){
 
+    const session = await getServerSession(authOptions);
     const cart = await getCart();
 
     return(
@@ -38,6 +42,7 @@ export default async function Navbar(){
                         </div>
                     </form>
                     <ShoppingCartButton cart={cart}/>
+                    <MenuButton session={session}/>
                 </div>
             </div>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Footer from './Footer';
 import Navbar from './Navbar/Navbar';
 import './globals.css'
-
 import { Inter } from 'next/font/google'
+import  SessionProvider from './SessionProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SessionProvider>
         <Navbar />
         <main className='p-4 max-w-7xl m-auto min-w-[300px]'>
         {children}
         </main>
         <Footer/>
+        </SessionProvider>
         </body>
     </html>
   );
