dda5e52b2858bfd227c04f90a58f5ce7091c5dca
MaxiPrantera <maximiliano.prantera@gmail.com>
2023-10-23 12:39:39 -0300

    post actualizacion

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,12 +1,29 @@
 "use client";
+import Link from 'next/link';
 import { CartItemWithProduct } from '../../lib/db/cart';
 import Image from 'next/image';
+import { formatPrice } from '@/lib/format';
+import { useTransition } from 'react';
 
 interface CartEntryProps {
     cartItem: CartItemWithProduct,
+    setProductQuantity: (productId: string, quantity: number) => Promise<void>
 }
 
-export default function CartEntry({cartItem: {product, quantity}}: CartEntryProps){
+export default function CartEntry({cartItem: {product, quantity}, setProductQuantity,}: CartEntryProps){
+
+    const [isPending, startTransition] = useTransition();
+
+    const quantityOptions : JSX.Element[] = [];
+    for(let i = 1; i < 99; i++){
+        quantityOptions.push(
+            <option value={i} key={i}>
+                {i}
+            </option>
+        );
+    }
+
+
     return(
         <div>
             <div className='flex flex-wrap items-center gap-3'>
@@ -17,6 +34,32 @@ export default function CartEntry({cartItem: {product, quantity}}: CartEntryProp
                 alt={product.name}
                 className='rounded-lg'
                 />
+                <div>
+                    <Link href={"/products/" + product.id} className='font-bold'>
+                        {product.name}
+                    </Link>
+                    <div>Price: {formatPrice(product.price)}</div>
+                    <div className='my-1 flex items-center gap-2'>
+                        Quantity:
+                        <select
+                        className='select select-bordered w-full max-w-[80px]'
+                        defaultValue={quantity}
+                        onChange={e => {
+                            const newQuantity = parseInt(e.currentTarget.value)
+                            startTransition(async () => {
+                                await setProductQuantity(product.id, newQuantity)
+                            })
+                        }}
+                        >
+                            <option value={0}>0 (Remove)</option>
+                            {quantityOptions}
+                        </select>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        Total: {formatPrice(product.price * quantity)}
+                        {isPending && <span className='loading loading-spinner loading-sm' />}
+                    </div>
+                </div>
             </div>
             <div className='divider'/>
         </div>
