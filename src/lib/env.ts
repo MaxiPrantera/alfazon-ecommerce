ab89f86963ab5de0b8c8eac15bcc4212bab7e568
MaxiPrantera <maximiliano.prantera@gmail.com>
2023-10-25 15:35:47 -0300

    fix zod nonempty

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,15 +2,15 @@ import zod from "zod";
 
 const envSchema = zod.object({
     
-    DATABASE_URL: zod.string().nonempty(),
+    DATABASE_URL: zod.string().min(1),
 
-    GOOGLE_CLIENT_ID:zod.string().nonempty(),
+    GOOGLE_CLIENT_ID: zod.string().min(1),
 
-    GOOGLE_CLIENT_SECRET:zod.string().nonempty(),
+    GOOGLE_CLIENT_SECRET: zod.string().min(1),
 
-    NEXTAUTH_URL:zod.string().nonempty(),
+    NEXTAUTH_URL: zod.string().min(1),
 
-    NEXTAUTH_SECRET:zod.string().nonempty(),
+    NEXTAUTH_SECRET: zod.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
